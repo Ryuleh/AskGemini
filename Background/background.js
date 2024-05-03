bbcf3a25a44c9f6cbf66505cb9cae7033c74b6d3
Jerry Lin <103540592+NotJerwee@users.noreply.github.com>
2024-05-03 14:18:26 -0400

    Update background.js

--- Background/background.js
+++ Background/background.js
@@ -14,8 +14,8 @@ chrome.runtime.onMessage.addListener(
 
   chrome.runtime.onInstalled.addListener(async () => {
       chrome.contextMenus.create({
-        id: 'OpenAskGemini',
-        title: "Open AskGemini",
+        id: 'OpenAskGPT',
+        title: "Open AskGPT",
         type: 'normal'
       });
   });
